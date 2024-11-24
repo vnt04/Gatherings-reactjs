4588f91a3e82da1f473c5b7e0ad32bce88255583
vnt04 <84673577+vnt04@users.noreply.github.com>
2024-11-24 14:26:33 +0700

    set up project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-500 text-4xl">This is Gathering App </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/layouts/Headers.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div className="text-red-100">Header</div>;
+}
+
+export default Header;
